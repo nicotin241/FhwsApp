eb251d7402267507e07020c5520cd23ce71fd963
Alexander Aries <alexander.aries@maindreieck.com>
2017-09-04 17:47:52 +0200

    Fixed Busplan-crash at first start on Android 7; App-Icon optimized

--- app/src/main/java/de/android/fhwsapp/busplaene/BusplanDataFetcher.java
+++ app/src/main/java/de/android/fhwsapp/busplaene/BusplanDataFetcher.java
@@ -3,6 +3,7 @@ package de.android.fhwsapp.busplaene;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -26,17 +27,12 @@ public class BusplanDataFetcher extends AsyncTask<Void, Void, Void> {
     private Database database;
     private Context mContext;
 
-    private HashMap<String, String> map;
-    private ListView listView;
-
-
     private String urlString = "http://54.93.76.71:8080/FHWS/busplan";
 
-    public BusplanDataFetcher(Context context, HashMap<String,String> map, ListView listView) {
+    public BusplanDataFetcher(Context context) {
 
         mContext = context;
-        this.map = map;
-        this.listView = listView;
+
     }
 
     @Override
@@ -56,12 +52,20 @@ public class BusplanDataFetcher extends AsyncTask<Void, Void, Void> {
 
             URL url = new URL(urlString);
             urlConnection = (HttpURLConnection) url.openConnection();
-            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                result.append(line);
+            if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    result.append(line);
+                }
+
+            } else {
+
+                Toast.makeText(mContext, "Busplan-Serverfehler: " + urlConnection.getResponseCode() + "-" + urlConnection.getResponseMessage(), Toast.LENGTH_SHORT).show();
+
             }
 
         } catch (Exception e) {
@@ -76,11 +80,8 @@ public class BusplanDataFetcher extends AsyncTask<Void, Void, Void> {
 
             JSONArray jsonArray = new JSONArray(serverData);
 
-            if(jsonArray != null)
-                try {
-                    database.deleteOldBusLinien();
-                } catch (Exception e) {
-                }
+            if (jsonArray != null)
+                database.deleteOldBusLinien();
 
             for (int i = 0; i < jsonArray.length(); i++) {
 
@@ -92,17 +93,15 @@ public class BusplanDataFetcher extends AsyncTask<Void, Void, Void> {
                 String url = "";
                 url = linien.getString(linie);
 
-
-                try {
-                    database.addBusplan(linie, url);
-                } catch (Exception e) {
-                }
+                database.addBusplan(linie, url);
 
             }
 
         } catch (JSONException e) {
 
             e.printStackTrace();
+
+        } catch (Exception e) {
 
         }
 
@@ -112,19 +111,9 @@ public class BusplanDataFetcher extends AsyncTask<Void, Void, Void> {
     @Override
     protected void onPostExecute(Void aVoid) {
 
-        map = database.getBusLinien();
-
-        String[] lineNames = map.keySet().toArray(new String[map.keySet().size()]);
-        BuslinienListAdapter adapter = new BuslinienListAdapter(mContext, lineNames);
-        listView.setAdapter(adapter);
-
-        listView.deferNotifyDataSetChanged();
-
-
         super.onPostExecute(aVoid);
 
     }
-
 
 }
 
